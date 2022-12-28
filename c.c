27b10d2bf328c0b68c3710d1c7b3804db5df4334
Joyce-Zo <109672607+Joyce-Zo@users.noreply.github.com>
2022-12-29 00:37:18 +0800

    Add files via upload

--- c.c
+++ c.c
@@ -1,2 +1,48 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
+#include"Stack.h"
+
+
+void STtest1()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	
+	//printf("%d ", StackPop(&st));
+	StackDestroy(&st); 
+}
+
+void STtest2()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+
+
+
+	//printf("%d ", StackPop(&st));
+	StackDestroy(&st);
+
+}
+int main()
+{
+	//STtest1();
+	STtest2();
+}
